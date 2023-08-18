6852795b0c76766aea236e9a12c1b87122079b79
Jubair <jubair.ahmed2838@gmail.com>
2023-08-19 01:22:46 +0600

    validation is not added yet

--- index.js
+++ index.js
@@ -22,6 +22,17 @@ function setInnerHTMLArea(areaId, area) {
     return result;
 
 }
+
+function addAreaToLog(shapeNameId, area) {
+    const p = document.createElement('p'),
+          areaLog = document.getElementById('area-log'),
+          count = areaLog.childElementCount,
+          shapeName = document.getElementById(shapeNameId).innerText;  
+    p.innerHTML = `${count}. ${shapeName}: ${area}cm<sup>2</sup> <button class="btn btn-sm text-white btn-success ml-2">Convert <p>m<sup>2</sup></p></button>`;
+    p.className = 'font-semibold my-2 px-2 text-left';
+    areaLog.appendChild(p);
+
+}
 // Reuseable functions ends
 
 // Calculate the area of triangle
@@ -30,6 +41,8 @@ function calculateTriangleArea() {
           height = getInputValue('triangle-height'),
           area = 0.5 * base * height;
     setInnerHTMLArea('triangle-area', area)
+    // add area to the log
+    addAreaToLog('triangle', area);
 }
 
 // calculate the area of rectangle
@@ -37,7 +50,9 @@ function calculateRectangleArea() {
     const width = getInputValue('rectangle-width'),
           length = getInputValue('rectangle-length'),
           area = width * length;
-    setInnerHTMLArea('rectangle-area', area);      
+    setInnerHTMLArea('rectangle-area', area);
+    // add area to the log
+    addAreaToLog('rectangle', area);   
 }
 
 // calculate the area of parallelogram
@@ -45,7 +60,9 @@ function calculateParallelogramArea() {
     const base = getInputValue('parallelogram-base'),
           height = getInputValue('parallelogram-height'),
           area = base * height;
-    setInnerHTMLArea('parallelogram-area', area);      
+    setInnerHTMLArea('parallelogram-area', area);
+    // add area to the log
+    addAreaToLog('parallelogram', area);   
 }
 
 // calculate the area of rhombus
@@ -53,7 +70,9 @@ function calculateRhombusArea() {
     const diagonalOne = getInputValue('rhombus-diagonal-one'),
           diagonalTwo = getInputValue('rhombus-diagonal-two'),
           area = diagonalOne * diagonalTwo;
-    setInnerHTMLArea('rhombus-area', area);      
+    setInnerHTMLArea('rhombus-area', area);
+    // add area to the log
+    addAreaToLog('rhombus', area);       
 }
 
 // calculate the area of pentagon
@@ -62,4 +81,16 @@ function calculatePentagonArea() {
           apothem = getInputValue('pentagon-apothem'),
           area = 0.5 * perimeter * apothem;
     setInnerHTMLArea('pentagon-area', area);
+    // add area to the log
+    addAreaToLog('pentagon', area); 
+}
+
+// calculate the area of ellipse
+function calculateEllipseArea() {
+    const majorRadius = getInputValue('major-radius'),
+          minorRadius = getInputValue('minor-radius'), 
+          area = (Math.PI * majorRadius * minorRadius).toFixed(2);
+    setInnerHTMLArea('ellipse-area', area);
+    // add area to the log
+    addAreaToLog('ellipse', area); 
 }
