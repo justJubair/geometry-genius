468a6fd193240daf5c66ad9af63fc0d791cfc600
Jubair <jubair.ahmed2838@gmail.com>
2023-08-18 23:36:34 +0600

    overall html structure

---
+++ index.js
@@ -0,0 +1,34 @@
+// Reuseable functions
+// get input value from input field
+function getInputValue(inputId) {
+    const value = parseFloat(document.getElementById(inputId).value);
+    return value;
+}
+
+// set innerText of an element
+function setInnerText(inputId, area) {
+    const element = document.getElementById(inputId);
+    element.innerText = area;
+}
+
+// set dynamic area function
+function setInnerHTMLArea(areaId, area) {   
+    // create p element
+    const p = document.createElement('p'),
+          areaBox = document.getElementById(areaId);
+    p.innerHTML = `Area: ${area} cm<sup>2</sup>`;
+    p.className = 'font-bold'
+    const result = areaBox.appendChild(p);
+    return result;
+
+}
+// Reuseable functions ends
+
+// Calculate the area of triangle
+function calculateTriangleArea() {
+    const base = getInputValue('triangle-base'),
+          height = getInputValue('triangle-height'),
+          area = 0.5 * base * height;
+    setInnerHTMLArea('triangle-area', area)
+}
+
